eb32256038d31f2bbdbaa86aee40564fab7ceb51
Hudson111-s <145516182+Hudson111-s@users.noreply.github.com>
2026-06-16 18:56:26 -0400

    Replace arphdr struct with ether_arp

--- src/arp.c
+++ src/arp.c
@@ -1,14 +1,14 @@
 #include <stdio.h>
 #include <stdint.h>
 #include <stddef.h>
-#include <net/if_arp.h>
+#include <netinet/if_ether.h>
 
 #include "arp.h"
 
 int parse_arp(uint8_t *buffer, size_t size) {
-    if (sizeof(struct arphdr) > size) return -1;
+    if (sizeof(struct ether_arp) > size) return -1;
 
-    struct arphdr* arp = (struct arphdr *)buffer;
+    struct ether_arp* arp = (struct ether_arp *)buffer;
     (void)arp; // Placeholder
 
     printf("--- (ARP) ---\n"); // Placeholder
